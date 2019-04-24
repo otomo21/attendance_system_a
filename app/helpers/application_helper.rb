ad4cc8998b76d3af1904e9e6c267e5080ad9fb04
otomo21 <k1bbs2str@gmail.com>
2019-04-24 05:53:19 +0000

    Finish Additional-items-1

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+  
+  # 表示色の変更土曜日は青、日曜日は赤
+  def day_color(wday)
+    if wday == 0
+      "sunday"
+    elsif wday == 6
+      "saturday"
+    end
+  end
 end
